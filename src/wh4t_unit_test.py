a81c3d4e0aeaf0a0fef82241fee4f7160b4874de
hernani <hernani@clementine.vecirex.net>
2012-07-18 18:59:36 +0200

    Add starting point for a unit test; start enhancing OO between document.py & documents.py

---
+++ src/wh4t_unit_test.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python2.6
+# -*- coding: utf-8 -*-
+"""
+@author Hernani Marques <h2m@access.uzh.ch>, 2012
+"""
+
+from wh4t.documents import collection
+from wh4t.settings import printLine
+
+# Program starts here
+xmlDocuments = collection()
+xmlDocument = xmlDocuments.getDoc(42) # Get doc #42 for tests
+
+# Print raw content, tokens and then types (mixed- and lowercase)
+printLine()
+print "Print raw content: "
+print xmlDocument.getRawContent()
+printLine()
+print "Print tokens: "
+print xmlDocument.getTokens()
+printLine()
+print "Print types (mixed-case): "
+print xmlDocument.getTypes()
+printLine()
+print "Print types (lower-case): "
+print xmlDocument.getTypes(lower=True)
+printLine()
+print "Print words: "
+print xmlDocument.getWords()
+printLine()
+print "Print stems: "
+print xmlDocument.getStems()
+printLine()
